2b8db45c89a4d657b21358fb312c1c8a3be1417b
ivy799 <muhammadraihanaan123@gmail.com>
2026-01-07 11:40:37 +0800

    FIX : Fsinxing Prisma Client Config

--- src/lib/prismaClient.ts
+++ src/lib/prismaClient.ts
@@ -1,17 +1,24 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
 declare global {
   // eslint-disable-next-line no-var
   var __prisma: PrismaClient | undefined
 }
 
+const connectionString = process.env.DATABASE_URL!
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
+  prisma = new PrismaClient({ adapter })
 } else {
   if (!global.__prisma) {
-    global.__prisma = new PrismaClient()
+    global.__prisma = new PrismaClient({ adapter })
   }
   prisma = global.__prisma
 }
